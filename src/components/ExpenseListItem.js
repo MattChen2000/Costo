b5c00ccaa05b574a5f79cdce4a379c9a27dbfef9
MattChen2000 <qchen1@haverford.edu>
2020-01-17 18:09:55 -0500

    Fix expense list item time display issue

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import moment from 'moment';
 
-const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createAt }) => (
     <Link to={`/edit/${id}`} className="list-item">
         <div>
             <h3 className="list-item__title">{description}</h3>
-            <span className="list-item__sub-title">{moment(createdAt).format('MMMM Do, YYYY')}</span>
+            <span className="list-item__sub-title">{moment(createAt).format('MMMM Do, YYYY')}</span>
         </div>
         <h3 className="list-item__data">{numeral(amount / 100).format('$0,0.00')}</h3>
     </Link>
